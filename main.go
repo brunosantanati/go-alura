0f0903eb8b9c25ecfe7a61db0f9e0258c9df02ec
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2023-03-01 08:10:42 -0300

    Add struct Produto e gerar tabela dinamica na view

--- main.go
+++ main.go
@@ -5,6 +5,13 @@ import (
 	"net/http"
 )
 
+type Produto struct {
+	Nome       string
+	Descricao  string
+	Preco      float64
+	Quantidade int
+}
+
 //para rodar esse projeto: go run main.go
 
 var tmpl = template.Must(template.ParseGlob("templates/*.html"))
@@ -15,5 +22,12 @@ func main() {
 }
 
 func Index(w http.ResponseWriter, r *http.Request) {
-	tmpl.ExecuteTemplate(w, "Index", nil)
+	produtos := []Produto{
+		{Nome: "Camiseta", Descricao: "Azul, bem bonita", Preco: 39, Quantidade: 5},
+		{"Tênis", "Confortável", 89, 3},
+		{"Fone", "Muito bom", 59, 2},
+		{"Produto novo", "Muito legal", 1.99, 1},
+	}
+
+	tmpl.ExecuteTemplate(w, "Index", produtos)
 }
